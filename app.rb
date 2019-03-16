934e542f3707dab7e573b184c306e68657b13860
Jiahao Li <reg@ljh.me>
2019-03-16 19:00:46 -0400

    Renames `schedule` param to `mbta_schedule`

--- app.rb
+++ app.rb
@@ -3,5 +3,5 @@ require 'sinatra'
 require_relative './mbta'
 
 get '/' do
-  haml :index, format: :html5, locals: {schedule: MBTA.get_schedule}
+  haml :index, format: :html5, locals: {mbta_schedule: MBTA.get_schedule}
 end
