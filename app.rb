e79ec8339db7eab99aafa0b17a518b9a9b084ac9
Jiahao Li <reg@ljh.me>
2019-03-16 19:04:32 -0400

    Adds the view part of Bluebikes integration

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 
 require_relative './mbta'
+require_relative './bluebikes'
 
 get '/' do
-  haml :index, format: :html5, locals: {mbta_schedule: MBTA.get_schedule}
+  haml :index, format: :html5, locals: {
+    mbta_schedule: MBTA.get_schedule,
+    bluebikes_status: Bluebikes.get_status,
+  }
 end
---
+++ bluebikes.rb
@@ -0,0 +1,5 @@
+class Bluebikes
+  def self.get_status
+    return {'Ashdown': {bikes: 1, docks: 14}}
+  end
+end
