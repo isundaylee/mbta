89a5f0fb618fd3a66977db3383b6f3c9cb72ddd0
Jiahao Li <reg@ljh.me>
2019-03-18 20:09:56 -0400

    Adds Bluebikes data caching

--- bluebikes.rb
+++ bluebikes.rb
@@ -3,11 +3,15 @@ require 'http'
 
 class Bluebikes
   DATA_URL = 'https://gbfs.bluebikes.com/gbfs/en/station_status.json'
+  CACHE_THRESHOLD = 3.0
+
+  @@mu = Mutex.new
+
+  @@updated_at = nil
+  @@cached_data = nil
 
   def self.get_station_status(station_id)
-    data = HTTP.get(DATA_URL).parse
-
-    data['data']['stations'].each do |station|
+    get_data['data']['stations'].each do |station|
       if station['station_id'].to_i == station_id
         return {
           bikes: station['num_bikes_available'].to_i,
@@ -18,4 +22,16 @@ class Bluebikes
 
     nil
   end
+
+  private
+    def self.get_data
+      @@mu.synchronize do
+        if @@updated_at.nil? || (Time.now - @@updated_at > CACHE_THRESHOLD)
+          @@updated_at = Time.now
+          @@cached_data = HTTP.get(DATA_URL).parse
+        end
+
+        @@cached_data
+      end
+    end
 end
